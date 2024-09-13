ece549522997e22baa549d9142867ae5773cb0b2
Lucas <lucas@informatique-m2i.fr>
2024-09-13 16:38:37 +0200

    create EmployeeBasketZone

---
+++ app/Models/EmployeeBasketZone.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class EmployeeBasketZone extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'employee_id',
+        'zone_id',
+        'employee_basket_zone_charged'
+    ];
+
+    public function employee(): BelongsTo
+    {
+        return $this->belongsTo(Employee::class);
+    }
+
+    public function zone(): BelongsTo
+    {
+        return $this->belongsTo(BasketZone::class);
+    }
+}

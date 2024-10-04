2c217069a6dee0c69f2ee711d16891dd42010440
Lucas <lucas@informatique-m2i.fr>
2024-10-04 15:05:30 +0200

    add user resource and role table/seeder role_user table

---
+++ app/Filament/Resources/UserResource.php
@@ -0,0 +1,104 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\UserResource\Pages;
+use App\Filament\Resources\UserResource\RelationManagers;
+use App\Models\Role;
+use App\Models\User;
+use Filament\Forms;
+use Filament\Forms\Components\Select;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class UserResource extends Resource
+{
+    protected static ?string $model = User::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\TextInput::make('name')
+                    ->label('Nom')
+                    ->required()
+                    ->placeholder('John Doe'),
+                Forms\Components\TextInput::make('firstname')
+                    ->label('Prénom')
+                    ->required()
+                    ->placeholder('John'),
+                Forms\Components\TextInput::make('email')
+                    ->label('Email')
+                    ->required()
+                    ->placeholder('john@example.com'),
+                Select::make('roles')
+                    ->label('Rôles')
+                    ->relationship('roles', 'name')  // Spécifie la relation many-to-many avec la table Role
+                    ->options(Role::all()->pluck('name', 'id')) // Charge tous les rôles disponibles depuis la table roles
+                    ->multiple()
+                    ->placeholder('Sélectionner les rôles')
+                    ->required(),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                TextColumn::make('name')
+                    ->label('Nom')
+                    ->searchable()
+                    ->sortable(),
+                TextColumn::make('firstname')
+                    ->label('Prénom')
+                    ->searchable()
+                    ->sortable(),
+                TextColumn::make('email')
+                    ->label('Email')
+                    ->searchable()
+                    ->sortable(),
+                TextColumn::make('roles')
+                    ->label('Rôles')
+                    ->formatStateUsing(function ($state, $record) {
+                        return $record->roles->pluck('name')->join(', ');
+                    })
+                    ->sortable(),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+/*                Tables\Actions\ViewAction::make(),*/
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListUsers::route('/'),
+            'create' => Pages\CreateUser::route('/create'),
+            'view' => Pages\ViewUser::route('/{record}'),
+            'edit' => Pages\EditUser::route('/{record}/edit'),
+        ];
+    }
+}

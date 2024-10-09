5eeb003ed6ac67c68fed3f699d48ecccf9f028b9
Lucas <lucas@informatique-m2i.fr>
2024-10-09 15:31:32 +0200

    add new page summary.blade.php in dashboard

--- resources/views/components/dashboard/summary.blade.php
+++ resources/views/components/dashboard/summary.blade.php
@@ -1,4 +1,4 @@
-{{--project.blade.php--}}
+{{--summary.blade.php--}}
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">

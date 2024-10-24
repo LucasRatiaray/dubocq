9889f196881cd5c87a07b235bbc51df94827a39b
Lucas <lucas@informatique-m2i.fr>
2024-10-24 10:43:50 +0200

    update home page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,7 +5,7 @@
         </h2>
     </x-slot>
 
-    <div class="py-12">
+{{--    <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white dark:bg-customGrayLight overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900 dark:text-gray-100 flex gap-2">
@@ -16,5 +16,94 @@
                 </div>
             </div>
         </div>
+    </div>--}}
+    <!-- ====== Hero Section Start -->
+    <div
+        class="relative dark:bg-dark pt-[120px] pb-[110px] lg:pt-[150px] max-w-7xl mx-auto sm:px-6 lg:px-8"
+    >
+        <div class="container mx-auto">
+            <div class="flex flex-wrap items-center -mx-4">
+                <div class="w-full px-4 lg:w-5/12">
+                    <div class="hero-content">
+                        <h1
+                            class="mb-5 text-4xl font-bold !leading-[1.208] text-dark dark:text-white sm:text-[42px] lg:text-[40px] xl:text-5xl"
+                        >
+                            Dubocq <span class="text-customColor">Pointage</span> Gérez vos heures efficacement.
+                        </h1>
+                        <p
+                            class="mb-8 max-w-[480px] text-base text-body-color dark:text-dark-6"
+                        >
+                            Simplifiez la gestion des heures de travail de vos équipes grâce à notre solution de pointage sur mesure, conçue pour répondre aux besoins spécifiques de chaque projet. Que vous soyez en déplacement ou au bureau, suivez en temps réel les heures travaillées, gérez les chantiers et optimisez la productivité de vos salariés.
+                        </p>
+
+                        <div class="clients pt-16">
+                            <h6
+                                class="flex items-center mb-6 text-xs font-normal text-body-color dark:text-dark-6"
+                            >
+                                Votre partenaire informatique :
+                                <span class="inline-block w-8 h-px ml-3 bg-body-color"></span>
+                            </h6>
+                            <div class="flex items-center gap-4 xl:gap-[50px]">
+                                <a href="javascript:void(0)" class="block py-3">
+                                    <img
+                                        src="/images/logo-m2i.png"
+                                        alt="M2i"
+                                        class="w-36"
+                                    />
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="hidden px-4 lg:block lg:w-1/12"></div>
+                <div class="w-full px-4 lg:w-6/12">
+                    <div class="lg:ml-auto lg:text-right">
+                        <div class="relative z-10 inline-block pt-11 lg:pt-0">
+                            <img
+                                src="/images/constructors.jpg"
+                                alt="hero"
+                                class="max-w-full lg:ml-auto rounded-3xl"
+                            />
+                            <span class="absolute -left-8 -bottom-8 z-[-1]">
+                     <svg
+                         width="93"
+                         height="93"
+                         viewBox="0 0 93 93"
+                         fill="none"
+                         xmlns="http://www.w3.org/2000/svg"
+                     >
+                        <circle cx="2.5" cy="2.5" r="2.5" fill="#049ce3" />
+                        <circle cx="2.5" cy="24.5" r="2.5" fill="#049ce3" />
+                        <circle cx="2.5" cy="46.5" r="2.5" fill="#049ce3" />
+                        <circle cx="2.5" cy="68.5" r="2.5" fill="#049ce3" />
+                        <circle cx="2.5" cy="90.5" r="2.5" fill="#049ce3" />
+                        <circle cx="24.5" cy="2.5" r="2.5" fill="#049ce3" />
+                        <circle cx="24.5" cy="24.5" r="2.5" fill="#049ce3" />
+                        <circle cx="24.5" cy="46.5" r="2.5" fill="#049ce3" />
+                        <circle cx="24.5" cy="68.5" r="2.5" fill="#049ce3" />
+                        <circle cx="24.5" cy="90.5" r="2.5" fill="#049ce3" />
+                        <circle cx="46.5" cy="2.5" r="2.5" fill="#049ce3" />
+                        <circle cx="46.5" cy="24.5" r="2.5" fill="#049ce3" />
+                        <circle cx="46.5" cy="46.5" r="2.5" fill="#049ce3" />
+                        <circle cx="46.5" cy="68.5" r="2.5" fill="#049ce3" />
+                        <circle cx="46.5" cy="90.5" r="2.5" fill="#049ce3" />
+                        <circle cx="68.5" cy="2.5" r="2.5" fill="#049ce3" />
+                        <circle cx="68.5" cy="24.5" r="2.5" fill="#049ce3" />
+                        <circle cx="68.5" cy="46.5" r="2.5" fill="#049ce3" />
+                        <circle cx="68.5" cy="68.5" r="2.5" fill="#049ce3" />
+                        <circle cx="68.5" cy="90.5" r="2.5" fill="#049ce3" />
+                        <circle cx="90.5" cy="2.5" r="2.5" fill="#049ce3" />
+                        <circle cx="90.5" cy="24.5" r="2.5" fill="#049ce3" />
+                        <circle cx="90.5" cy="46.5" r="2.5" fill="#049ce3" />
+                        <circle cx="90.5" cy="68.5" r="2.5" fill="#049ce3" />
+                        <circle cx="90.5" cy="90.5" r="2.5" fill="#049ce3" />
+                     </svg>
+                  </span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
+    <!-- ====== Hero Section End -->
 </x-app-layout>
